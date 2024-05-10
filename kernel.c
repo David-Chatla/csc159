155a6aca72da3a3d5a7c3c6204deee815de3edf5
David-Chatla <159684341+David-Chatla@users.noreply.github.com>
2024-05-10 02:15:20 -0700

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,231 +1,117 @@
-#include <spede/flames.h>   // for breakpoint()
-#include <spede/stdarg.h>   // for variable argument functions (va_*)
-#include <spede/stdio.h>    // for printf
-#include <spede/string.h>   // string handling
-
+/**
+ * CPE/CSC 159 - Operating System Pragmatics
+ * California State University, Sacramento
+ *
+ * Keyboard Functions
+ */
+#include "io.h"
 #include "kernel.h"
-#include "vga.h"
 #include "keyboard.h"
 
-#ifndef KERNEL_LOG_LEVEL_DEFAULT
-#define KERNEL_LOG_LEVEL_DEFAULT KERNEL_LOG_LEVEL_TRACE
-#endif
-
-// Current log level
-int kernel_log_level = KERNEL_LOG_LEVEL_DEFAULT;
+char ascii_values[] = {                                                 
+      '\0', '\001', '\002', '\003', '\004', '\005', '\006', '\007',                         
+     '\010', '\011', '\012', '\013', '\014', '\015', '\016', '\017',                       
+      '\020', '\021', '\022', '\023', '\024', '\025', '\026', '\027',                    
+     '\030', '\031', '\032', '\033', '\034', '\035', '\036', '\037',                        
+       ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',      
+     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',              
+       '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',            
+      'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',                 
+       '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',              
+      'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\177'                                        
+  }; 
 
 /**
- * Initializes any kernel internal data structures and variables
+ * Initializes keyboard data structures and variables
  */
-void kernel_init(void) {
-    // Display a welcome message on the host
-    kernel_log_info("Welcome to %s!", OS_NAME);
-
-    kernel_log_info("Initializing kernel...");
+void keyboard_init() {
+    kernel_log_info("Initializing keyboard driver");
 }
 
 /**
- * Prints a kernel log message to the host with an error log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
+ * Scans for keyboard input and returns the raw character data
+ * @return raw character data from the keyboard
  */
-void kernel_log_error(char *msg, ...) {
-    if (kernel_log_level < KERNEL_LOG_LEVEL_ERROR)
-        return;
+unsigned int keyboard_scan(void) {    
+    unsigned int c = inportb(0x60); // Read from keyboard port
+    if (c == 0xFFFFFFFF) { // Check for error (0xFFFFFFFF indicates failure)
+        kernel_log_error("Error reading from keyboard port");
+        return KEY_NULL;
+    }
+    kernel_log_info("keyboard_scan() returns: %10x\n", c);                  
+    return c;                         
+}    
 
-    va_list args;
-    va_start(args, msg);
-    printf("error: ");
-    vprintf(msg, args);
-    va_end(args);
-    printf("\n");
+/**
+ * Polls for a keyboard character to be entered.
+ *
+ * If a keyboard character data is present, will scan and return
+ * the decoded keyboard output.
+ *
+ * @return decoded character or KEY_NULL (0) for any character
+ *         that cannot be decoded
+ */
+unsigned int keyboard_poll(void) {   
+   unsigned int c = KEY_NULL;        
+    if((c = keyboard_scan()) != KEY_NULL)                                           
+   {                                
+        c = keyboard_decode(c);                                   
+    }                                                             
+    return c;                                                     
 }
 
 /**
- * Prints a kernel log message to the host with a warning log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
+ * Blocks until a keyboard character has been entered
+ * @return decoded character entered by the keyboard or KEY_NULL
+ *         for any character that cannot be decoded
  */
-void kernel_log_warn(char *msg, ...) {
-    if (kernel_log_level < KERNEL_LOG_LEVEL_WARN)
-        return;
-
-    va_list args;
-    va_start(args, msg);
-    printf("warn: ");
-    vprintf(msg, args);
-    va_end(args);
-    printf("\n");
+unsigned int keyboard_getc(void) {
+    unsigned int c = KEY_NULL;
+    while ((c = keyboard_poll()) == KEY_NULL);
+    return c;
 }
 
 /**
- * Prints a kernel log message to the host with an info log level
+ * Processes raw keyboard input and decodes it.
  *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
+ * Should keep track of the keyboard status for the following keys:
+ *   SHIFT, CTRL, ALT, CAPS, NUMLOCK
+ *
+ * For all other characters, they should be decoded/mapped to ASCII
+ * or ASCII-friendly characters.
+ *
+ * For any character that cannot be mapped, KEY_NULL should be returned.
+ *
+ * If *all* of the status keys defined in KEY_KERNEL_DEBUG are pressed,
+ * while another character is entered, the kernel_debug_command()
+ * function should be called.
  */
-void kernel_log_info(char *msg, ...) {
-    if (kernel_log_level < KERNEL_LOG_LEVEL_INFO) {
-        return;
+unsigned int keyboard_decode(unsigned int c) {
+    // Check if the key pressed is a special function key
+    if (c & 0x80) {
+        // Key release event, ignore for now
+        return KEY_NULL;
     }
 
-    va_list args;
-    va_start(args, msg);
-    printf("info: ");
-    vprintf(msg, args);
-    va_end(args);
-    printf("\n");
-}
-
-/**
- * Prints a kernel log message to the host with a debug log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_debug(char *msg, ...) {
-    if (kernel_log_level < KERNEL_LOG_LEVEL_DEBUG)
-        return;
-
-    va_list args;
-    va_start(args, msg);
-    printf("debug: ");
-    vprintf(msg, args);
-    va_end(args);
-    printf("\n");
-}
-
-/**
- * Prints a kernel log message to the host with a trace log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_trace(char *msg, ...) {
-    if (kernel_log_level < KERNEL_LOG_LEVEL_TRACE)
-        return;
-
-    va_list args;
-    va_start(args, msg);
-    printf("trace: ");
-    vprintf(msg, args);
-    va_end(args);
-    printf("\n");
-}
-
-/**
- * Triggers a kernel panic that does the following:
- *   - Displays a panic message on the host console
- *   - Triggers a breakpoint (if running through GDB)
- *   - aborts/exits the operating system program
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_panic(char *msg, ...) {
-    va_list args;
-    va_start(args, msg);
-    printf("Panic: ");
-    vprintf(msg, args);
-    va_end(args);
-
-    // Trigger a breakpoint to inspect what caused the panic
-    kernel_break();
-
-    // Exit since this is fatal
-    exit(1);
-}
-
-/**
- * Returns the current log level
- * @return the kernel log level
- */
-int kernel_get_log_level(void) {
-    return kernel_log_level;
-}
-
-/**
- * Sets the new log level and returns the value set
- * @param level - the log level to set
- * @return the kernel log level
- */
-int kernel_set_log_level(log_level_t level) {
-    int prev_log_level = kernel_log_level;
-
-    if (level < 0) {
-        kernel_log_level = 0;
-    } else if (level > 4) {
-        kernel_log_level = 4;
-    } else {
-        kernel_log_level = level;
+    // Check for modifier keys (SHIFT, CTRL, ALT)
+    // For now, we'll ignore these and simply return the ASCII value
+    if (c >= 0x80 && c <= 0x9C) {
+        // Key is a special key, return KEY_NULL
+        return KEY_NULL;
     }
 
-    if (prev_log_level != kernel_log_level) {
-        printf("<<kernel log level set to %d>>", kernel_log_level);
-    }
-
-    return kernel_log_level;
-}
-
-/**
- * Triggers a breakpoint (if running under GBD)
- */
-void kernel_break(void) {
-    breakpoint();
-}
-
-/**
- * Triggers a kernel command
- */
-void kernel_command(char c) {
-    static int kernel_escape = 0;
-
+    // Check for non-alphanumeric keys
     switch (c) {
-        case 'p':
-        case 'P':
-            // Test the kernel panic
-            kernel_panic("test panic");
-            break;
-
-        case 'b':
-        case 'B':
-            // Test a breakpoint (only valid when running with GDB)
-            kernel_break();
-            break;
-
-        // Add new commands to:
-        //  - Clear the screen (k)
-        //  - Increase the kernel log level (+)
-        //  - Decrease the kernel log level (-)
-
-        case KEY_ESCAPE:
-            // Exit the OS if we press escape three times in a row
-            kernel_escape++;
-
-            if (kernel_escape >= 3) {
-                kernel_exit();
-            }
-            break;
-
-        case KEY_NULL:
-        default:
-            kernel_escape = 0;
-            // Nothing to do
-            break;
+        case 0x1C: return '\n';  // Enter key
+        case 0x0E: return '\b';  // Backspace key
+        // Add more cases for special keys as needed
     }
-}
 
-/**
- * Exits the kernel
- */
-void kernel_exit(void) {
-    // Print to the terminal
-    printf("Exiting %s...\n", OS_NAME);
+    // Check if it's a printable ASCII character
+    if (c < sizeof(ascii_values)) {
+        return ascii_values[c];
+    }
 
-    // Print to the VGA display
-    vga_printf("Exiting %s...\n", OS_NAME);
-    // Exit
-    exit(0);
+    // Key is not mapped to a printable ASCII character
+    return KEY_NULL;
 }
